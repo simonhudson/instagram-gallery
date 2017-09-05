cd24dfa7ff723126d86ef2172a41671979dc7204
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-09-05 08:51:07 +0100

    Trying to get fetch() working

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,20 @@
-const getMedia = () => {
+let returnData;
+const getMedia = (options) => {
+	options = options || {};
+	const count = options.count || '';
 
+	const request = {
+		url: `https://api.instagram.com/v1/users/self/media/recent/?access_token=1400621600.75f2cc1.57c8f0dec0564e43929bd3b09dea5152&count=${count}`,
+		mode: 'cors'
+	};
+	fetch(request.url)
+	    .then(blob => blob.json())
+	    .then(data => console.log(data));
 };
 
 const init = () => {
 	const gallery = document.querySelector('.js-gallery');
 	if (!gallery) return;
+	getMedia();
 };
 init();
