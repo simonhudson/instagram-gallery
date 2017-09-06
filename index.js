5bcb7443fefc187be98e040f332d58dda9e62195
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-09-06 08:58:56 +0100

    Add paths config

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+require('dotenv').config()
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+require('./config/paths')(app);
+
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.engine('.hbs', exphbs({
 	defaultLayout: 'main',
@@ -13,8 +18,9 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (request, response) => {
-	response.render('index');
+
+app.get('/', (req, res) => {
+	res.render('index');
 });
 
 app.listen(port, (err) => {
