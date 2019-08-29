9da46b1d1123e3cbbdab330d275440566223d35b
Simon Hudson <simon.hudson@experian.com>
2019-08-29 08:35:14 +0100

    yeoman scaffold

---
+++ src/App.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import React, { Component } from 'react';
+import './assets/css/styles.scss';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Hello world!</h1>
+            </>
+        );
+    }
+}
+
+export default App;
